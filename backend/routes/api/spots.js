4b70f72ded9e149648ba85aea7d324950e8fb673
Andrew McCord <a2011mccord@gmail.com>
2023-11-04 16:32:33 -0400

    test

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -71,50 +71,50 @@ const validateSpotInfo = [
   handleValidationErrors
 ];
 
-router.get('/', async (req, res, next) => {
-  const spots = await Spot.findAll({
-    include: [
-      {
-        model: SpotImage
-      },
-      {
-        model: Review
-      }
-    ]
-  });
+// router.get('/', async (req, res, next) => {
+//   const spots = await Spot.findAll({
+//     include: [
+//       {
+//         model: SpotImage
+//       },
+//       {
+//         model: Review
+//       }
+//     ]
+//   });
 
-  let spotsList = []
-  spots.forEach(spot => {
-    spotsList.push(spot.toJSON())
-  });
+//   let spotsList = []
+//   spots.forEach(spot => {
+//     spotsList.push(spot.toJSON())
+//   });
 
-  spotsList.forEach(spot => {
-    let ratingsSum = 0;
-    spot.Reviews.forEach(review => {
-      ratingsSum += review.stars
-    });
-    if (ratingsSum) {
-      spot.avgRating = ratingsSum / spot.Reviews.length;
-    } else {
-      spot.avgRating = "No reviews for this spot yet"
-    };
-    delete spot.Reviews;
-  });
+//   spotsList.forEach(spot => {
+//     let ratingsSum = 0;
+//     spot.Reviews.forEach(review => {
+//       ratingsSum += review.stars
+//     });
+//     if (ratingsSum) {
+//       spot.avgRating = ratingsSum / spot.Reviews.length;
+//     } else {
+//       spot.avgRating = "No reviews for this spot yet"
+//     };
+//     delete spot.Reviews;
+//   });
 
-  spotsList.forEach(spot => {
-    spot.SpotImages.forEach(image => {
-      if (image.preview === true) {
-        spot.previewImage = image.url
-      };
-    });
-    if (!spot.previewImage) {
-      spot.previewImage = "No preview image found"
-    };
-    delete spot.SpotImages
-  });
+//   spotsList.forEach(spot => {
+//     spot.SpotImages.forEach(image => {
+//       if (image.preview === true) {
+//         spot.previewImage = image.url
+//       };
+//     });
+//     if (!spot.previewImage) {
+//       spot.previewImage = "No preview image found"
+//     };
+//     delete spot.SpotImages
+//   });
 
-  res.json(spotsList);
-});
+//   res.json(spotsList);
+// });
 
 router.get('/:spotId/bookings', requireAuth, async (req, res, next) => {
   const spot = await Spot.findByPk(req.params.spotId);
