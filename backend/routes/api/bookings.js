9d47a932ede2c67caf40c3506256d804e93d79ea
Andrew McCord <a2011mccord@gmail.com>
2023-11-03 23:12:46 -0400

    finished Get all of the Current User's Bookings endpoint

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -9,7 +9,39 @@ const { Spot, SpotImage, Review, User, ReviewImage, Booking } = require('../../d
 const router = express.Router();
 
 router.get('/current', requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const bookings = await Booking.findAll({
+    where: { userId: user.id },
+    include: [
+      {
+        model: Spot,
+        attributes: { exclude: [ 'description', 'createdAt', 'updatedAt' ]},
+        include: { model: SpotImage }
+      }
+    ]
+  });
 
+  let bookingsList = [];
+  bookings.forEach(review => {
+    bookingsList.push(review.toJSON());
+  })
+
+  bookingsList.forEach(review => {
+    review.Spot.SpotImages.forEach(image => {
+      if (image.preview === true) {
+        review.Spot.previewImage = image.url
+      };
+    });
+    if (!review.Spot.previewImage) {
+      review.Spot.previewImage = "No preview image found"
+    };
+    delete review.Spot.SpotImages
+  });
+
+  const response = {
+    "Bookings": bookingsList
+  }
+  res.json(response);
 });
 
 module.exports = router;
