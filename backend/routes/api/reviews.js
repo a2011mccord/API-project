ae8731f48634f7c06bdbad7db48bef6078dc28e2
Andrew McCord <a2011mccord@gmail.com>
2023-11-03 20:35:16 -0400

    finished Get all Reviews of the Current User endpoint

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -4,12 +4,52 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { Op } = require('sequelize');
 
 const { requireAuth, authorize } = require('../../utils/auth');
-const { Spot, SpotImage, Review, User } = require('../../db/models');
+const { Spot, SpotImage, Review, User, ReviewImage } = require('../../db/models');
 
 const router = express.Router();
 
 router.get('/current', requireAuth, async (req, res, next) => {
+  const { user } = req;
+  const reviews = await Review.findAll({
+    where: { userId: user.id },
+    include: [
+      {
+        model: User,
+        attributes: [ 'id', 'firstName', 'lastName' ]
+      },
+      {
+        model: Spot,
+        attributes: { exclude: [ 'description', 'createdAt', 'updatedAt' ]},
+        include: { model: SpotImage }
+      },
+      {
+        model: ReviewImage,
+        attributes: { exclude: [ 'createdAt', 'updatedAt' ]}
+      }
+    ]
+  });
 
+  let reviewsList = [];
+  reviews.forEach(review => {
+    reviewsList.push(review.toJSON());
+  })
+
+  reviewsList.forEach(review => {
+    review.Spot.SpotImages.forEach(image => {
+      if (image.preview === true) {
+        review.Spot.previewImage = image.url
+      };
+    });
+    if (!review.Spot.previewImage) {
+      review.Spot.previewImage = "No preview image found"
+    };
+    delete review.Spot.SpotImages
+  });
+
+  const response = {
+    "Reviews": reviewsList
+  }
+  res.json(response);
 });
 
 module.exports = router
