b70105658f75f967f840d80598886cd2a8cf12a6
Andrew McCord <a2011mccord@gmail.com>
2024-01-13 16:37:36 -0500

    nested login and signup modals inside the profile button

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,34 +1,10 @@
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
-import OpenModalButton from "../OpenModalButton";
-import LoginFormModal from "../LoginFormModal";
-import SignupFormModal from "../SignupFormModal";
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
-
-  const sessionLinks = sessionUser ? (
-    <li>
-      <ProfileButton user={sessionUser} />
-    </li>
-  ) : (
-    <>
-      <li>
-        <OpenModalButton
-          buttonText='Log In'
-          modalComponent={<LoginFormModal />}
-        />
-      </li>
-      <li>
-        <OpenModalButton
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal />}
-        />
-      </li>
-    </>
-  );
 
   return (
     <nav>
@@ -36,7 +12,11 @@ function Navigation({ isLoaded }) {
         <li>
           <NavLink to='/'>Home</NavLink>
         </li>
-        {isLoaded && sessionLinks}
+        {isLoaded && (
+          <li>
+            <ProfileButton user={sessionUser} />
+          </li>
+        )}
       </ul>
     </nav>
   )
