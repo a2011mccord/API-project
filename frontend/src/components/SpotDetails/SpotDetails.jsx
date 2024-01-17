1de79f20cb9ac972dca9759203ab14c768347e6c
Andrew McCord <a2011mccord@gmail.com>
2024-01-16 20:13:33 -0500

    spot details page basic structure finished

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -17,11 +17,46 @@ function SpotDetails() {
   }, [dispatch, spotId])
 
   return (
-    <>
-      <div>
+    <div className="details-wrapper">
+      <header>
         <h1>{spotDetails.name}</h1>
+        <h3>{`${spotDetails.city}, ${spotDetails.state}, ${spotDetails.country}`}</h3>
+      </header>
+      <div className="images-wrapper">
+        <div id="preview-image">1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
       </div>
-      <div>
+      <section>
+        <h2>{`Hosted by ${spotDetails.Owner.firstName} ${spotDetails.Owner.lastName}`}</h2>
+        <p>{spotDetails.description}</p>
+      </section>
+      <div className="reserve">
+        <div className="price">{`$${spotDetails.price} night`}</div>
+        <div className="ratings">
+          <i className="fa-solid fa-star" />
+          {spotDetails.avgStarRating !== "No reviews for this spot yet" ?
+            spotDetails.avgStarRating : "N/A"}
+          {' '}&#x2022;{' '}
+          {`${spotDetails.numReviews} ${spotDetails.numReviews > 1 ? "reviews" : "review"}`}
+        </div>
+        <button
+          onClick={() => alert("Feature coming soon...")}
+        >Reserve</button>
+      </div>
+
+      <div className="bar" />
+
+      <div className="reviews-wrapper">
+        <div className="ratings">
+          <i className="fa-solid fa-star" />
+          {spotDetails.avgStarRating !== "No reviews for this spot yet" ?
+            spotDetails.avgStarRating : "N/A"}
+          {' '}&#x2022;{' '}
+          {`${spotDetails.numReviews} ${spotDetails.numReviews > 1 ? "reviews" : "review"}`}
+        </div>
         {
           spotReviews && spotReviews.map(review => {
             return (
@@ -34,7 +69,7 @@ function SpotDetails() {
           })
         }
       </div>
-    </>
+    </div>
   )
 }
 
