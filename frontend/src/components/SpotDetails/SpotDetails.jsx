25de50d26e77128e4dbdd40e8db7944a24d890ea
Andrew McCord <a2011mccord@gmail.com>
2024-01-17 00:10:43 -0500

    fixed small bug in SpotDetails.jsx

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -10,6 +10,7 @@ function SpotDetails() {
   const { spotId } = useParams();
   const spotDetails = useSelector(state => state.spotsState.spotDetails);
   const spotReviews = useSelector(selectReviewsArray);
+  console.log(spotDetails)
 
   useEffect(() => {
     dispatch(fetchSpotDetails(spotId));
@@ -17,59 +18,63 @@ function SpotDetails() {
   }, [dispatch, spotId])
 
   return (
-    <div className="details-wrapper">
-      <header>
-        <h1>{spotDetails.name}</h1>
-        <h3>{`${spotDetails.city}, ${spotDetails.state}, ${spotDetails.country}`}</h3>
-      </header>
-      <div className="images-wrapper">
-        <div id="preview-image">1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-      </div>
-      <section>
-        <h2>{`Hosted by ${spotDetails.Owner.firstName} ${spotDetails.Owner.lastName}`}</h2>
-        <p>{spotDetails.description}</p>
-      </section>
-      <div className="reserve">
-        <div className="price">{`$${spotDetails.price} night`}</div>
-        <div className="ratings">
-          <i className="fa-solid fa-star" />
-          {spotDetails.avgStarRating !== "No reviews for this spot yet" ?
-            spotDetails.avgStarRating : "N/A"}
-          {' '}&#x2022;{' '}
-          {`${spotDetails.numReviews} ${spotDetails.numReviews > 1 ? "reviews" : "review"}`}
-        </div>
-        <button
-          onClick={() => alert("Feature coming soon...")}
-        >Reserve</button>
-      </div>
+    <>
+      {spotDetails &&
+        <div className="details-wrapper">
+          <header>
+            <h1>{spotDetails.name}</h1>
+            <h3>{`${spotDetails.city}, ${spotDetails.state}, ${spotDetails.country}`}</h3>
+          </header>
+          <div className="images-wrapper">
+            <div id="preview-image">1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+          </div>
+          <section>
+            <h2>{`Hosted by ${spotDetails.Owner?.firstName} ${spotDetails.Owner?.lastName}`}</h2>
+            <p>{spotDetails.description}</p>
+          </section>
+          <div className="reserve">
+            <div className="price">{`$${spotDetails.price} night`}</div>
+            <div className="ratings">
+              <i className="fa-solid fa-star" />
+              {spotDetails.avgStarRating !== "No reviews for this spot yet" ?
+                spotDetails.avgStarRating : "N/A"}
+              {' '}&#x2022;{' '}
+              {`${spotDetails.numReviews} ${spotDetails.numReviews > 1 ? "reviews" : "review"}`}
+            </div>
+            <button
+              onClick={() => alert("Feature coming soon...")}
+            >Reserve</button>
+          </div>
 
-      <div className="bar" />
+          <div className="bar" />
 
-      <div className="reviews-wrapper">
-        <div className="ratings">
-          <i className="fa-solid fa-star" />
-          {spotDetails.avgStarRating !== "No reviews for this spot yet" ?
-            spotDetails.avgStarRating : "N/A"}
-          {' '}&#x2022;{' '}
-          {`${spotDetails.numReviews} ${spotDetails.numReviews > 1 ? "reviews" : "review"}`}
+          <div className="reviews-wrapper">
+            <div className="ratings">
+              <i className="fa-solid fa-star" />
+              {spotDetails.avgStarRating !== "No reviews for this spot yet" ?
+                spotDetails.avgStarRating : "N/A"}
+              {' '}&#x2022;{' '}
+              {`${spotDetails.numReviews} ${spotDetails.numReviews > 1 ? "reviews" : "review"}`}
+            </div>
+            {
+              spotReviews && spotReviews.map(review => {
+                return (
+                  <div key={review.id}>
+                    <h3>{review.User.firstName}</h3>
+                    <span>{review.createdAt}</span>
+                    <p>{review.review}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
-        {
-          spotReviews && spotReviews.map(review => {
-            return (
-              <div key={review.id}>
-                <h3>{review.User.firstName}</h3>
-                <span>{review.createdAt}</span>
-                <p>{review.review}</p>
-              </div>
-            )
-          })
-        }
-      </div>
-    </div>
+      }
+    </>
   )
 }
 
